18e8a8b657cce8b388b048034aa56019abf82479
Csalt4544 <104047849+Csalt4544@users.noreply.github.com>
2023-12-06 22:23:49 -0500

    Update Converter.py
    Changed to accept command line arguments rather than specific files.

--- Converter.py
+++ Converter.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 # SeqIO from Biopython was imported to aid in converting the FASTQ file to FASTA
-
+import sys
 from Bio import SeqIO
+
+original_file = sys.argv[1]
+converted_file = sys.argv[2]
 
 # the original FASTQ is opened while a new file with .fasta extension is created in write mode
 # line is parsed with SeqIO.parse() using for loop
 # SeqIO.write() used to convert each line to FASTA format
 
-with open("SRS301869.denovo_duplicates_marked.trimmed.1.fastq", "r") as fastq_file:
-    with open("SRS301869.denovo_duplicates_marked.trimmed.1.fasta", "w") as fasta_file:
+with open(original_file, "r") as fastq_file:
+    with open(converted_file, "w") as fasta_file:
         for line in SeqIO.parse(fastq_file, "fastq"):
             SeqIO.write(line, fasta_file, "fasta")
